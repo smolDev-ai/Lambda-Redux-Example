70917690609b14ff7832fb9eee2941369c2f72b5
Megan <megan.miller@smoldev.dev>
2020-06-20 22:16:06 -0500

    Backend fleshed out

--- server/data/migrations/20200616173017_posts.js
+++ server/data/migrations/20200616173017_posts.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("posts", tbl => {
+      tbl.increments("id"),
+      tbl.string("title").notNullable(),
+      tbl.integer("author_id").references("id").inTable("users").onUpdate("CASCADE").onDelete("CASCADE"),
+      tbl.text("body").notNullable(),
+      tbl.timestamp('created_at').defaultTo(knex.fn.now());
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('posts')
 };
